fc32dbe3756356f7c2459ff2951c60c9de45ff56
CarlosJulianCogolloGonzalez <44674949+CarlosJulianCogolloGonzalez@users.noreply.github.com>
2018-11-01 10:08:47 -0500

    Add files via upload

---
+++ programacion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Binarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int est =2;
+            int x =0;
+            int A = 0;
+            int D = 0;
+            int I = 0;
+            int [] vectordebinarios = new int[21];
+            string linea;
+            Console.Write("Ingrese un Numero:");
+            linea = Console.ReadLine();
+             x = int.Parse(linea);
+            D = 1;
+            while ((x >= est))
+            {
+                A = x % est;
+                vectordebinarios[D] = A;
+                D = D + 1;
+                x = x / est;
+            }
+            vectordebinarios [D] = x;
+            
+            for (I = D; I >= 1;I += -1)
+            {
+                Console.Write(vectordebinarios[I] + " ");
+            }
+           
+        }
+    }
+}
